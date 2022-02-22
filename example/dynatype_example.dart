5726f38598655200078189bd28934c570ea5a259
Chetan Patil <chetanpatil@Chetans-MacBook-Pro.local>
2022-02-23 02:38:42 +0530

    Example Update

--- example/dynatype_example.dart
+++ example/dynatype_example.dart
@@ -1,6 +1,27 @@
 import 'package:dynatype/dynatype.dart';
 
+///This is My json List which i will receive in response or request.
+///You can try removing fields and text it.
+Map<String, dynamic> json = {
+  "book": {"name": "The alchemist", "author": "Paulo Coelho"},
+  "language": "english",
+  "publish": 1988
+};
+
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  ///Here i am defining the fields i required.
+  final List<DynaType> required = [
+    DynaType(key: "book", type: Map<String, dynamic>),
+    DynaType(key: "language", type: String),
+    DynaType(key: "publish", type: int),
+  ];
+
+  ///this is the [DynaType.verifyFields()] function which returns the missing, null and wrong data type Keys.
+  DynaFields mainKey = DynaType.verifyFields(
+    data: json,
+    presence: required,
+  );
+
+  ///Here is the output
+  print(mainKey.notFound.keys);
 }
